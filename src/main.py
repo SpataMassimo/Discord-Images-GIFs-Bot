e6240a6fee3b4f8f206df8265ba266131f860315
SpataMassimo <spatamassimo@alice.it>
2022-08-09 06:25:21 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,16 +8,12 @@ TOKEN="Your token"
 google_search_api_key = "YOUR API KEY" 
 giphy_search_api_key = "YOUR API KEY"
 
-#~ TOKEN = "MTAwMzA3NDU4NzQ3MjQ0MTM5NA.GA94nd.dWMYO-wn9N4RywyL56FZrAUYfv-1Rh-GR7MS9A" 
-#~ google_search_api_key = "AIzaSyDrw6XvXQbcYfHQWW54GHL3Us0Fj7IrkBA"
-#~ giphy_search_api_key = "lbm3JP1gQRXcOejzr9D9DBSkyU0MeOSL"
-
 def main():
     client = commands.Bot(command_prefix="$")
     
     @client.command(aliases=["show"])
     async def show_image(ctx, *, search):
-        url =  ((build("customsearch", "v1", developerKey=google_search_api_key).cse()).list(q=f"{search}", cx="51d03d688da4c4c2f", searchType="image").execute())["items"][0]["link"]
+        url =  ((build("customsearch", "v1", developerKey=google_search_api_key).cse()).list(q=f"{search}", cx="YOUR GOOGLE ID", searchType="image").execute())["items"][0]["link"]
         await ctx.send(embed = (discord.Embed(title=f"This is the image ({search}) you reserch|").set_image(url=url)))
         api_instance = giphy_client.DefaultApi()
         try:
