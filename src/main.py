f8d13c088ec100ee73075082b75bf48361abe0c5
SpataMassimo <spatamassimo@alice.it>
2022-08-09 06:05:04 +0200

    fixed:  Completed and fixed the code related to the following files: src/main.py, test/conftest.py, test/main_test.py, .github/workflows/ci.yml,  requirement.txt, setup.cfg
    In the file main.py we have eliminate the function on_ready (unnecessary functionality) and add function init().
    In the file conftest.py we have inserted the @pytest.fixture. in the file main_test we have 2 function to test (test_show_image and test_init)
    In the requirement.txt add one more library (pytest-asyncio). With setup.cnf added some features for pytest coverage.

--- src/main.py
+++ src/main.py
@@ -1,34 +1,34 @@
-import os
-import random
 import discord
+import giphy_client
 from discord.ext import commands
 from googleapiclient.discovery import build
+from giphy_client.rest import ApiException
 
-TOKEN = "*Your token*"   #Bot Token
-api_key = "*Your api-key*"  #Google API key
-# cx is Google Search Engine ID
+TOKEN="Your token"
+google_search_api_key = "YOUR API KEY" 
+giphy_search_api_key = "YOUR API KEY"
+
+#~ TOKEN = "MTAwMzA3NDU4NzQ3MjQ0MTM5NA.GA94nd.dWMYO-wn9N4RywyL56FZrAUYfv-1Rh-GR7MS9A" 
+#~ google_search_api_key = "AIzaSyDrw6XvXQbcYfHQWW54GHL3Us0Fj7IrkBA"
+#~ giphy_search_api_key = "lbm3JP1gQRXcOejzr9D9DBSkyU0MeOSL"
 
 def main():
     client = commands.Bot(command_prefix="$")
-
-    @client.event
-    async def on_ready():
-        print("!!! Bot Is Online !!!\n")
-
-
+    
     @client.command(aliases=["show"])
-    async def showpic(ctx, *, search):
-        ran = random.randint(0, 9)
-        resource = build("customsearch", "v1", developerKey=api_key).cse()
-        result = resource.list(
-            q=f"{search}", cx="*your cx*", searchType="image"
-        ).execute()
-        url = result["items"][ran]["link"]
-        embed1 = discord.Embed(title=f"This is the image ({search}) you reserch|")
-        embed1.set_image(url=url)
-        await ctx.send(embed=embed1)
-
+    async def show_image(ctx, *, search):
+        url =  ((build("customsearch", "v1", developerKey=google_search_api_key).cse()).list(q=f"{search}", cx="51d03d688da4c4c2f", searchType="image").execute())["items"][0]["link"]
+        await ctx.send(embed = (discord.Embed(title=f"This is the image ({search}) you reserch|").set_image(url=url)))
+        api_instance = giphy_client.DefaultApi()
+        try:
+            lst = list((api_instance.gifs_search_get(giphy_search_api_key, q=f"{search}", limit=1, rating="g")).data)
+            embed = discord.Embed(title=f"This is the gif ({search}) you reserch|").set_image(url=f'https://media.giphy.com/{lst[0].id}/giphy.gif')
+            await ctx.send(lst[0].embed_url)
+        except ApiException as e:
+            print("Exception when calling API!")
     client.run(TOKEN)
+def init():
+    if __name__ == '__main__':
+        main() 
 
-if __name__ == "__main__":
-    main()
+init()
--- test/conftest.py
+++ test/conftest.py
@@ -5,48 +5,19 @@ import discord
 import discord.ext.commands as commands
 import discord.ext.test as test
 
-
-@pytest.fixture
-def client(event_loop):
-    c = discord.Client(loop=event_loop)
-    test.configure(c)
-    return c
-
-
 @pytest.fixture
 def bot(request, event_loop):
     intents = discord.Intents.default()
     intents.members = True
-    b = commands.Bot("!", loop=event_loop, intents=intents)
+    b = commands.Bot("$", loop=event_loop, intents=intents)
 
     marks = request.function.pytestmark
     mark = None
     for mark in marks:
         if mark.name == "cogs":
             break
-
     if mark is not None:
         for extension in mark.args:
-            b.load_extension("tests.internal." + extension)
-
+            b.load_extension("test.internal." + extension)
     test.configure(b)
     return b
-
-
-@pytest.fixture(autouse=True)
-async def cleanup():
-    yield
-    await test.empty_queue()
-
-
-def pytest_sessionfinish(session, exitstatus):
-    """ Code to execute after all tests. """
-
-    # dat files are created when using attachements
-    print("\n-------------------------\nClean dpytest_*.dat files")
-    fileList = glob.glob('./dpytest_*.dat')
-    for filePath in fileList:
-        try:
-            os.remove(filePath)
-        except Exception:
-            print("Error while deleting file : ", filePath)
